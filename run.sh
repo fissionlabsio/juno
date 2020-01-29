5c72ebd9b988aa6cfada661b8588911a2d6298e2
Kiryl <laptanovi4@gmail.com>
2020-01-29 22:07:15 +0300

    upd dockerfile and run.sh

--- run.sh
+++ run.sh
@@ -14,19 +14,19 @@ docker exec -ti juno_postgres psql -f /root/schema/block.sql -d $POSTGRES_DB_NAM
 docker exec -ti juno_postgres psql -f /root/schema/transaction.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
 
 # create docker.config.toml, put values from .env file to config.toml
-echo -n 'rpc_url="' >> docker.config.toml && echo -n $RPC_URL >> docker.config.toml && echo -n '"' >> docker.config.toml
-sed -i "/rpc/a client_node=\"$CLIENT_URL\"" docker.config.toml
-echo '[database]' >> docker.config.toml
-sed -i "/database/a host=\"$POSTGRES_DB_HOST\"" docker.config.toml
-sed -i "/host/a port=$POSTGRES_DB_PORT" docker.config.toml
-sed -i "/port/a name=\"$POSTGRES_DB_NAME\"" docker.config.toml
-sed -i "/name/a user=\"$POSTGRES_USER_NAME\"" docker.config.toml
-sed -i "/user/a password=\"$POSTGRES_DB_PASSWORD\"" docker.config.toml
-sed -i "/password/a ssl_mode=\"$JUNO_SSL_MODE\"" docker.config.toml
+echo -n 'rpc_url="' >> config.toml && echo -n $RPC_URL >> config.toml && echo -n '"' >> config.toml
+sed -i "/rpc/a client_node=\"$CLIENT_URL\"" config.toml
+echo '[database]' >> config.toml && echo '' >> config.toml
+sed -i "/database/a host=\"$POSTGRES_DB_HOST\"" config.toml
+sed -i "/host/a port=$POSTGRES_DB_PORT" config.toml
+sed -i "/port/a name=\"$POSTGRES_DB_NAME\"" config.toml
+sed -i "/name/a user=\"$POSTGRES_USER_NAME\"" config.toml
+sed -i "/user/a password=\"$POSTGRES_DB_PASSWORD\"" config.toml
+sed -i "/password/a ssl_mode=\"$JUNO_SSL_MODE\"" config.toml
 
 # build juno and run it in container
 docker build -t juno:latest --build-arg JUNO_WORKERS_NUMBER=$JUNO_WORKERS_NUMBER .
 docker run -d --name juno --network="host" juno:latest
 
 # remove config toml, as far as it copyed to container
-rm docker.config.toml
+rm config.toml
